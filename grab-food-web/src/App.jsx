a2ff91b7f5a8c9854063a236e2e6518d7889b3cc
NguynThuHigh <minhnguyen11a1cmg@gmail.com>
2024-04-08 00:48:50 +0700

    8th commit

--- grab-food-web/src/App.jsx
+++ grab-food-web/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes,Route,HashRouter} from 'react-router-dom';
+import { BrowserRouter, Routes,HashRouter as Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 
@@ -9,13 +9,13 @@ function App() {
   
   return (
     <div>
-      <HashRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path='/restaurant' element={<Restaurant></Restaurant>}/>
             <Route path='/searchRes' element={<Search_Res></Search_Res>}/>
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
 
   );
