b631185f505b292a0d05cefafabba982921c15dd
Nguyễn Minh Nguyên <87266672+nguynthuhigh@users.noreply.github.com>
2024-04-08 00:15:26 +0700

    6th commit in a days .-.

--- grab-food-web/src/App.jsx
+++ grab-food-web/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes,HashRouter as Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 
